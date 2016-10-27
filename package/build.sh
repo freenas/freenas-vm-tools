b0f793019a7a9fa2f004ef728630c6370ad730ae
Jakub Klama <jakub@ixsystems.com>
2016-10-27 04:10:43 +0200

    Add script to build .deb package.

---
+++ package/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PKGROOT=dest
+
+build_deb_systemd()
+{
+    install -d ${PKGROOT}/DEBIAN
+    install -d ${PKGROOT}/lib/systemd/system/
+    install -d ${PKGROOT}/usr/local/sbin
+    install -d ${PKGROOT}/usr/local/lib
+    install -d ${PKGROOT}/usr/local/lib/freenas-vm-tools
+    install deb/control ${PKGROOT}/DEBIAN/
+    install ${BUILD_ROOT}/freenas-vm-tools ${PKGROOT}/usr/local/sbin/
+    install ${BUILD_ROOT}/lib*.so ${PKGROOT}/usr/local/lib/freenas-vm-tools/
+    install ${SOURCE_ROOT}/systemd/freenas-vm-tools.service ${PKGROOT}/lib/systemd/system/
+    dpkg -b ${PKGROOT} freenas-vm-tools-systemd.deb
+}
+
+build_deb_systemd
