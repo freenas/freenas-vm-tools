867496ab8ce35938b66a5c729131f347e78ac20f
650elx <bprokop02@gmail.com>
2016-10-28 23:25:42 +0200

    Fix wrong indentation

--- package/build.sh
+++ package/build.sh
@@ -17,14 +17,14 @@ build_deb_package()
     if [ $1 = "systemd" ]; then
           install -d ${PKGROOT}/lib/systemd/system/ 
           install ${SOURCE_ROOT}/systemd/freenas-vm-tools.service ${PKGROOT}/lib/systemd/system/
-	  dpkg -b ${PKGROOT} freenas-vm-tools-systemd.deb
-	  rm -rf ${PKGROOT}
+          dpkg -b ${PKGROOT} freenas-vm-tools-systemd.deb
+          rm -rf ${PKGROOT}
 
     elif [ $1 = "upstart" ]; then
           install -d ${PKGROOT}/etc/init
           install ${SOURCE_ROOT}/upstart/freenas-vm-tools.conf ${PKGROOT}/etc/init
-	  dpkg -b ${PKGROOT} freenas-vm-tools-upstart.deb
-	  rm -rf ${PKGROOT}
+          dpkg -b ${PKGROOT} freenas-vm-tools-upstart.deb
+          rm -rf ${PKGROOT}
 
     fi
 }
